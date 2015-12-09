f65e917c6f763d644507769fccfc99917879d48b
Austin Paxton <austin.is.paxton@gmail.com>
2015-12-09 07:17:42 -0600

    Solve day 6 part 2 and refactor part 1

--- 6_2.rb
+++ 6_2.rb
@@ -1,15 +1,15 @@
-@grid = Hash.new false
+@grid = Hash.new 0
 
 def set(state, x1, y1, x2, y2)
   for x in x1..x2
     for y in y1..y2
       case state
       when 'on'
-        @grid["#{x},#{y}"] = true
+        @grid["#{x},#{y}"] += 1
       when 'off'
-        @grid["#{x},#{y}"] = false
+        @grid["#{x},#{y}"] -= 1 unless @grid["#{x},#{y}"] == 0
       when 'toggle'
-        @grid["#{x},#{y}"] = !@grid["#{x},#{y}"]
+        @grid["#{x},#{y}"] += 2
       end
     end
   end
@@ -25,5 +25,4 @@ instructions.split("\n").each do |instruction|
   set(i[0], i[1], i[2], i[3], i[4])
 end
 
-on = @grid.values - [false]
-p on.size
+p @grid.values.inject(:+)
