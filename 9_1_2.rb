5fdbf129b3054acdd27dd9910dcfa014f0a17dbb
Austin Paxton <austin.is.paxton@gmail.com>
2015-12-09 08:40:13 -0600

    solve day 9 parts 1 and 2

---
+++ 9_1_2.rb
@@ -0,0 +1,15 @@
+# fun with nested hashes
+distances = Hash.new { |h,k| h[k] = {} }
+
+File.read('9.input').each_line do |line|
+  # fun with regex
+  /(\w+) to (\w+) = (\d+)/.match line
+  # more fun with nested hashes
+  distances[$1][$2] = distances[$2][$1] = $3.to_i
+end
+
+puts distances.keys.permutation.map { |path|
+  p path.each_cons(2).reduce(0) do |distance, (current_town, next_town)|
+    distance + distances[current_town][next_town]
+  end
+}.minmax
