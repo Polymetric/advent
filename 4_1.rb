7bcb1f4c8071e0e2e8a21f4c44526f1207bfe33f
Austin Paxton <austin.is.paxton@gmail.com>
2015-12-09 04:36:36 -0600

    solutions to day 4

---
+++ 4_1.rb
@@ -0,0 +1,14 @@
+require 'digest'
+
+md5 = Digest::MD5.new
+
+num = 0
+
+while true
+  digest = md5.hexdigest('bgvyzdsv' + num.to_s)
+  if /^00000/.match(digest)
+    p num
+    p digest
+  end
+  num += 1
+end
